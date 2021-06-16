b77b23baa8d1eb97fc1ae0536a8672742fc71f78
Kamil <zhaudatovkamil@mail.ru>
2021-06-16 20:46:31 +0600

    calculator app

---
+++ src/calculator/calculator.js
@@ -0,0 +1,55 @@
+import './calculator.css'
+
+function Calculator() {
+    function insert(num) {
+        document.form.textview.value = 
+        document.form.textview.value + num;
+    }
+    
+    function clean() {
+        document.form.textview.value = ''
+    }
+    
+    function back() {
+        let exp = document.form.textview.value;
+        document.form.textview.value = exp.substring(0,exp.length-1)
+    }
+    
+    function equal() {
+        let exp = document.form.textview.value;
+        if (exp) (
+            document.form.textview.value = eval(exp)
+        )
+    }
+    return(
+        <div>
+            <div className="container">
+                <div className="item input">
+                    <form name="form">
+                        <input type="text" name="textview" readonly/>
+                    </form>
+                </div>
+                <div className="item clean" onClick={() => clean()}>C</div>
+                <div className="item back" onClick={() => back()}>&larr;</div>
+                <div className="item" onClick={() => insert('+')}>+</div>
+                <div className="item" onClick={() => insert('-')}>-</div>
+                <div className="item" onClick={() => insert('*')}>&times;</div>
+                <div className="item" onClick={() => insert('/')}>&divide;</div>
+                <div className="item" onClick={() => insert(7)}>7</div>
+                <div className="item" onClick={() => insert(8)}>8</div>
+                <div className="item" onClick={() => insert(9)}>9</div>
+                <div className="item" onClick={() => insert(4)}>4</div>
+                <div className="item" onClick={() => insert(5)}>5</div>
+                <div className="item" onClick={() => insert(6)}>6</div>
+                <div className="item" onClick={() => insert(1)}>1</div>
+                <div className="item" onClick={() => insert(2)}>2</div>
+                <div className="item" onClick={() => insert(3)}>3</div>
+                <div className="item equal" onClick={() => equal()}>=</div>
+                <div className="item zero" onClick={() => insert(0)}>0</div>
+                <div className="item" onClick={() => insert('.')}>.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
